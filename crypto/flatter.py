a30368a22041bf73e8b273d3aed1722b3c9f303d
Zoee <30841158+n-WN@users.noreply.github.com>
2025-07-29 00:34:58 +0800

    Create flatter.py

---
+++ crypto/flatter.py
@@ -0,0 +1,9 @@
+# https://tangcuxiaojikuai.xyz/post/ec560d26.html
+from re import findall
+from subprocess import check_output
+
+def flatter(M):
+    # compile https://github.com/keeganryan/flatter and put it in $PATH
+    z = "[[" + "]\n[".join(" ".join(map(str, row)) for row in M) + "]]"
+    ret = check_output(["flatter"], input=z.encode())
+    return matrix(M.nrows(), M.ncols(), map(int, findall(b"-?\\d+", ret)))
